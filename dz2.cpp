2487743377f5a597460c8f28893ad167d79f3a46
utrobin <patr.rus@gmail.com>
2016-10-04 13:26:14 +0300

    Update dz2.cpp

--- dz2.cpp
+++ dz2.cpp
@@ -1,63 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
+#include <assert.h>
 
-int main(int argc, char *argv[])
-{
-    setlocale(LC_ALL, "Rus");
-    
-    if(argc < 3)
-    {
-        printf("Вы забыли ввести матрицу. \n");
-        return 0;
-    }
-    
-    const char *mat1 = argv[1];
-    printf("Искомое слово: %s\n", mat1);
-    
+#define MAX_SIZE 100
+
+float **get_matrix(const char *name_file) {
     FILE *TextFile;
-    TextFile = fopen(argv[1], "r");
+    TextFile = fopen(name_file, "r");
     
-    if (TextFile == NULL)
-    {
-        printf("Файл не существует\n");
-        return 0;
-    }
+    assert(TextFile != NULL);
     
-    const int max_size = 100;
-    char str[max_size];
+    char str[MAX_SIZE];
     
     int line = 0;
-    int row = 0;
+    int column = 0;
     
     int i = 0;
-    while (i < 2 && fgets(str, max_size, TextFile) ) {
+    while (i < 2 && fgets(str, MAX_SIZE, TextFile) ) {
         if(i == 0) {
             line =  atoi(&str[0]);
         }
         else {
-            row =  atoi(&str[0]);
+            column =  atoi(&str[0]);
         }
         i++;
     }
     
-    printf("Искомое слово: %i\n", line);
-    printf("Искомое слово: %i\n", row);
+    float **matrix = (float**) malloc(line * sizeof(float*));
+    for (i = 0; i < line; i++) {
+        matrix[i] = (float*) malloc(column * sizeof(float));
+    }
     
-    float array[line][row];
-    i = 0;
-    while (fgets(str, max_size, TextFile)) {
-        puts(str);
 
-        char *pch = strtok(str, " ");
+    i = 0;
+    while (fgets(str, MAX_SIZE, TextFile)) {
+        char *current_number = strtok(str, " ");
         int j = 0;
-        while (pch != NULL)
-        {
-            array[i][j] = atof(pch);
-            pch = strtok(NULL, " ");
+        while (current_number != NULL) {
+            matrix[i][j] = atof(current_number);
+            current_number = strtok(NULL, " ");
             j++;
         }
         i++;
     }
+    
+    return matrix;
+}
+
+int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "Rus");
+    
+    if(argc < 3) {
+        printf("Вы забыли ввести матрицу. \n");
+        return 0;
+    }
+    
+    float **matrix1 = get_matrix(argv[1]);
+    float **matrix2 = get_matrix(argv[2]);
+    
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            printf("%5f ", matrix1[i][j]);
+        }
+        printf("\n");
+        free(matrix1[i]);
+    }
+    free(matrix1);
+    
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            printf("%5f ", matrix2[i][j]);
+        }
+        printf("\n");
+        free(matrix2[i]);
+    }
+    free(matrix2);
+
     return 0;
 }
